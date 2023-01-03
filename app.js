501db85020faf813b7c4989d0d3bd6ae53684d23
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2023-01-03 22:56:22 +0530

    Just need to add cron now

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const cron = require('node-cron');
 const SibApiV3Sdk = require('sib-api-v3-sdk');
 const { Configuration, OpenAIApi } = require('openai');
 const seed = require('./seed.json');
-// const Newsletter = require('./models/newsletter');
 
-// start a cron job for everyweek monday at 11:00 am
-
-// cron.schedule('0 6 * * 1', () => {
-//   console.log('running a task every week monday at 11:00 am');
-//   sendEmail();
-// });   
-
-// send email to template 5
 async function sendEmail() {
 
   //get data from openai
+  var items = [];
+  for(let i = 0; i < 4; i++){
+  var response = await getCompletion();
+  items.push(response);
+  }
+
 
   let defaultClient = SibApiV3Sdk.ApiClient.instance;
   let apiKey = defaultClient.authentications['api-key'];
@@ -32,14 +29,14 @@ async function sendEmail() {
     ],
     templateId: 2,
     params: {
-      title1: 'Vortex',
-      title2: 'Hello, this is a test email',
-      title3: 'Hello, this is a test email',
-      title4: 'Hello, this is a test email',
-      body1: bodies[0],
-      body2: bodies[1],
-      body3: bodies[2],
-      body4: bodies[3],
+      title1: items[0][0],
+      title2: items[1][0],
+      title3: items[2][0],
+      title4: items[3][0],
+      body1: items[0][1],
+      body2: items[1][1],
+      body3: items[2][1],
+      body4: items[3][1],
     },
   };
   apiInstance.sendTransacEmail(sendSmtpEmail).then(
@@ -50,38 +47,42 @@ async function sendEmail() {
 }
 
 // get completion from openai
-async function getCompletion() {
-  var bodies = [];
-  for (var i = 0; i < 4; i++) {
+var getCompletion = async () => {
+  var news = "";  
 
-    // get keys from seed object
-    var keys = Object.keys(seed);
-    // get random key from seed object
-    var randomKey = keys[Math.floor(Math.random() * keys.length)];
-    // get random prompt from seed object
-    var randomPrompt = seed[randomKey];
-    // get random number from seed[randomKey].length
-    var randomNumber = Math.floor(Math.random() * seed[randomKey].length);
-    // get random prompt from seed[randomKey]
-    var randomPrompt = seed[randomKey][randomNumber];
+  // write function to get 4 different random prompts and get completion from openai
+  // get keys from seed object
+  var keys = Object.keys(seed);
+  // get random key from seed object
+  var randomKey = keys[Math.floor(Math.random() * keys.length)];
+  // get random prompt from seed object
+  var randomPrompt = seed[randomKey];
+  // get random number from seed[randomKey].length
+  var randomNumber = Math.floor(Math.random() * seed[randomKey].length);
+  // get random prompt from seed[randomKey]
+  var randomPrompt = seed[randomKey][randomNumber];
 
-    const configuration = new Configuration({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
-    const openai = new OpenAIApi(configuration);
-    completion = openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: randomPrompt,
-      max_tokens: 4000,
-    }).then((data) => {
-      // console.log(data.data.choices[0].text);
-      bodies.push(data.data.choices[0].text);
-    });
-  }
-  // wait for 5 seconds
-  // await new Promise(r => setTimeout(r, 10000));
-  console.log(bodies);
-  return bodies;
-}
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
 
-getCompletion();
+  const openai = new OpenAIApi(configuration);
+
+  await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: randomPrompt,
+    max_tokens: 4000,
+  }).then((response) => {
+    news = response.data.choices[0].text;
+  });
+
+
+  // // create promise to stop execution for 2 minutes
+  // var promise = new Promise((resolve, reject) => {
+  //   setTimeout(() => resolve("done!"), 10000)
+  // });              
+
+  return [randomKey, news];
+};
+
+sendEmail();
